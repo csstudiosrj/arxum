18017c91a947d3ba0f6f60acd58b908a518a3177
csstudiosrj <cscomrj02@gmail.com>
2026-05-20 17:23:13 +0000

    redirecionamento de todos os projetos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/suite/:path*",
+        destination: "https://arxum-suite.vercel.app/suite/:path*",
+      },
+      {
+        source: "/crew/:path*",
+        destination: "https://arxum-crew.vercel.app/crew/:path*",
+      },
+      {
+        source: "/fest/:path*",
+        destination: "https://arxum-fest.vercel.app/fest/:path*",
+      },
+      {
+        source: "/calculadoras/:path*",
+        destination: "https://arxum-calculadoras.vercel.app/calculadoras/:path*",
+      },
+      {
+        source: "/poseidon/:path*",
+        destination: "https://arxum-poseidon.vercel.app/poseidon/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
